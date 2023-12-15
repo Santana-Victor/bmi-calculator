66748bdf549f17fe809ec9f9722354b19100f444
Victor Santana <santanavictor592@gmail.com>
2023-12-15 16:09:23 -0300

    feat: Add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import "./App.css";
+import Main from "./components/Main";
+import ContainerPrimary from "./components/ContainerPrimary";
+import ContainerSecundary from "./components/ContainerSecundary";
+import Title from "./components/Title";
+import FormUserData from "./components/FormUserData";
+import RenderUserBMI from "./components/RenderUserBMI";
+import RenderAboutBMI from "./components/RenderAboutBMI";
+
+import { useUserBMIStore } from "./store/UserBMIStore";
+import ContainerResult from "./components/ContainerResult";
 
 export default function App() {
+  const [userBMI, aboutBMI] = useUserBMIStore((state) => [
+    state.userBMI,
+    state.aboutBMI,
+  ]);
+
   return (
     <>
-      <h1>Olá, mundo!!</h1>
+      <Main>
+        <ContainerPrimary>
+          <ContainerSecundary>
+            <Title>Calculadora de IMC</Title>
+            <FormUserData />
+            <ContainerResult>
+              {userBMI !== "" && (
+                <RenderUserBMI>Seu IMC é de: {userBMI}</RenderUserBMI>
+              )}
+              {aboutBMI !== "" && <RenderAboutBMI>{aboutBMI}</RenderAboutBMI>}
+            </ContainerResult>
+          </ContainerSecundary>
+        </ContainerPrimary>
+      </Main>
     </>
   );
 }
